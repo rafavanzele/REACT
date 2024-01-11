cfcc5da3be50f2f67b0f3fecac9c001be57469dd
rafavanzele <rafa_vanzele@hotmail.com>
2024-01-11 17:28:15 -0200

    mais exercício de react no mod.01

--- 1-INTRO/hello-world/src/App.jsx
+++ 1-INTRO/hello-world/src/App.jsx
@@ -8,7 +8,8 @@ function App() {
 
   return (
     <>
-      <h1>Hello, World!</h1>    
+      <h1>Hello, World!</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolores nihil vel, velit similique placeat, magnam est iure soluta maxime sapiente impedit asperiores omnis, quasi nostrum cumque ducimus nisi! Porro! Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum porro similique optio repellendus maxime dicta. Aliquid illum culpa labore totam quae. Voluptatum debitis autem dolorem deleniti consequatur non error ipsa.</p>    
     </>
   )
 }
