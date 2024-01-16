09d1cb6ec94b275aed80abaa68637282a6729291
rafavanzele <rafa_vanzele@hotmail.com>
2024-01-16 18:32:10 -0200

    exercícios de funcionalidades ES6

--- ES6/script.js
+++ ES6/script.js
@@ -37,3 +37,63 @@ const name = 'Sueli'
 logName()
 
 console.log(name)
+
+
+
+
+//2 ARROW FUNCTION
+
+//função clássica
+const sum = function sum(a, b) {
+    return a + b
+}
+
+/*função com arrow function
+const arrowSum = (a,b) => {
+    return a + b
+} */
+
+//quando a função tem só uma linha
+const arrowSum = (a, b) => a + b
+
+console.log(sum(10, 5))
+console.log(arrowSum(20, 20))
+
+
+const greeting = (nome) => {
+    if (nome) {
+        return 'Olá ' + nome + '!'
+    } else {
+        return 'Olá'
+    }
+}
+
+console.log(greeting('Rafael'))
+console.log(greeting())
+
+
+
+
+//3 FILTER
+
+const array = [1, 2, 3, 4, 5]
+
+const highNumbers = array.filter((n) => {
+    if (n > 3) {
+        return n
+    }
+})
+
+console.log(highNumbers)
+
+
+const users = [
+    {name: 'Rafael', available: true},
+    {name: 'Pedro', available: false},
+    {name: 'João', available: true},
+    {name: 'Guilherme', available: false},
+]
+
+const availableUsers = users.filter((user) => user.available)
+
+console.log(availableUsers)
