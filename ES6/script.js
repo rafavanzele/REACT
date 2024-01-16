3497f9db37a2253117bedf8e23e991895cf75d36
rafavanzele <rafa_vanzele@hotmail.com>
2024-01-16 17:50:34 -0200

    alguns exemplos de ferramentas pra ES6

---
+++ ES6/script.js
@@ -0,0 +1,39 @@
+// VAR, LET e CONST
+
+var x = 10
+var y = 15
+
+if (y > 10) {
+    var x = 5
+    console.log(x)
+}
+
+console.log(x)
+
+
+let a = 10
+let b = 15
+
+if (b > 10) {
+    let a = 5
+    console.log(a)
+}
+
+console.log(a)
+
+
+for (let i = 0; i < 5; i++) {
+    console.log(i)
+}
+
+
+function logName() {
+    const name = 'Rafael'
+    console.log(name)
+}
+
+const name = 'Sueli'
+
+logName()
+
+console.log(name)
