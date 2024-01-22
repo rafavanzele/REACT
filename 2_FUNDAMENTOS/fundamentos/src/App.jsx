9c09828614726352b79bfa1d827a618e998ea5e4
rafavanzele <rafa_vanzele@hotmail.com>
2024-01-22 17:25:51 -0200

    exercício de importação de componentes

---
+++ 2_FUNDAMENTOS/fundamentos/src/App.jsx
@@ -0,0 +1,23 @@
+//COMPONENTS
+import FirstComponent from './components/FirstComponent'
+
+//CSS
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>FUNDAMENTOS DE REACT</h1>
+        <FirstComponent />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.jsx
@@ -0,0 +1,12 @@
+const FirstComponent = () => {
+
+    return (
+        <div>
+            <h2>MEU PRIMEIRO COMPONENTE</h2>
+        </div>
+    )
+
+}
+
+
+export default FirstComponent;
