f490904dec6608cb5141f9e0371176e81ed377b3
rafavanzele <rafa_vanzele@hotmail.com>
2024-01-25 17:52:03 -0200

    if ternário em react

--- 3_AVANCANDO_EM_REACT/avancando_em_react/src/components/ConditionalRender.jsx
+++ 3_AVANCANDO_EM_REACT/avancando_em_react/src/components/ConditionalRender.jsx
@@ -3,12 +3,30 @@ import { useState } from 'react'
 
 const ConditionalRender = () => {
   const [x] = useState(true)
+
+  const [name] = useState('Rafael')
   
     return (
     <div>
         <h1>Isso será exibido?</h1>
         {x && <p>Se x for 'TRUE', sim!</p>}
+
+
+        <h1>IF Ternário</h1>
+        {name === 'Rafael'? (
+            <div>
+                <p>O nome é Rafael</p>
+            </div>
+        ) : (
+            <div>
+                <p>Nome não encontrado!</p>
+            </div>
+        )}
+
+
     </div>
+
+
   )
 }
 
