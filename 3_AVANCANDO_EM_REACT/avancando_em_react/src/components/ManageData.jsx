7c4b844eb0105aacad378d35c4a39b06a3f3f9c5
rafavanzele <rafa_vanzele@hotmail.com>
2024-01-25 16:44:44 -0200

    primeiro exercício com hook

--- 3_AVANCANDO_EM_REACT/avancando_em_react/src/components/ManageData.jsx
+++ 3_AVANCANDO_EM_REACT/avancando_em_react/src/components/ManageData.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
+
+
 
 const ManageData = () => {
+  
+  let someData = 10;
+  console.log(someData)
+  
+  
+  const [number, setNumber] = useState(25)
+  
   return (
-    <div>ManageData</div>
+        
+    <div>
+      <div>
+        <p>Valor: {someData}</p> 
+        <button onClick={() => (someData = 15)}>Mudar variável</button>   
+      </div>  
+
+      <div>
+        <p>Outro Valor: {number}</p>
+        <button onClick={() => setNumber(55)}>Mudar Valor</button>
+      </div>
+
+    </div>
   )
 }
 
