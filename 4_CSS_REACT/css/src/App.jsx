16e1fbcacab852a89699311803ac5e5d17df02a6
rafavanzele <rafa_vanzele@hotmail.com>
2024-01-31 17:30:38 -0200

    css de componentes / inline / dinâmico / global

---
+++ 4_CSS_REACT/css/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+import './App.css'
+import MyComponents from './components/MyComponents'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const n = 20
+
+  const [name] = useState('Rafael')
+
+  return (
+    <>
+      <div>
+        {/*CSS Global */}
+        <h1>React com CSS</h1>
+        
+        
+        {/*CSS de componentes */}
+        <MyComponents/>
+
+        <p>Este é o parágrafo do jsx</p>
+
+        {/*CSS Inline */}
+        <p style={{color: 'yellow', padding: '30px', backgroundColor: 'red'}}>Este elemento foi estilizado de forma inline</p>
+
+
+        {/*CSS Inline Dinâmico*/}
+        <h2 style={n < 15 ? ({color: 'green'}) : ({color: 'purple'})}>CSS Inline Dinâmico</h2>
+
+        <h2 style={n > 15 ? ({color: 'green'}) : ({color: 'purple'})}>CSS Inline Dinâmico</h2>
+
+        <h2 style={name === 'Rafael' ? ({color: 'green'}) : ({color: 'purple'})}>CSS Inline Dinâmico - TESTE NOME</h2>
+
+      </div>
+
+      
+      
+    </>
+  )
+}
+
+export default App
---
+++ 4_CSS_REACT/css/src/components/MyComponents.jsx
@@ -0,0 +1,15 @@
+import './MyComponents.css'
+
+const MyComponents = () => {
+  return (
+    <div>
+        <h1>CSS de Componentes</h1>
+        <p>Este é o parágrafo do componente</p>
+
+        <p className="my-component-paragraph">Este parágrafo também é do componente</p>
+
+    </div>
+  )
+}
+
+export default MyComponents
