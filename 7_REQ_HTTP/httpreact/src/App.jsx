d8de1932bd11fab06e486c22dfa1464261e303c1
rafavanzele <rafa_vanzele@hotmail.com>
2024-02-21 17:13:35 -0300

    resgatando dados da API

--- 7_REQ_HTTP/httpreact/src/App.jsx
+++ 7_REQ_HTTP/httpreact/src/App.jsx
@@ -1,12 +1,38 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
+const url = "http://localhost:3000/products"
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([])
+
+  //1- RESGATANDO DADOS
+  useEffect(() => {
+    
+    async function fetchData() {
+      const res = await fetch(url)
+
+      const data = await res.json()
+
+      setProducts(data)
+    }
+
+      fetchData()
+
+  }, [])
+
+  
 
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
+
+      <ul>
+        {products.map((product) =>
+          <li key={product.id}> {product.name} - R${product.price}</li>
+        )}
+      </ul>
+
     </div>
   )
 }
