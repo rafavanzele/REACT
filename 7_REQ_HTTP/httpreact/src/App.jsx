6d9e5866a0b4c45056fe9e28bee3e1dc4242e5ce
rafavanzele <rafa_vanzele@hotmail.com>
2024-02-22 17:29:42 -0300

    add novos dados em react/http

--- 7_REQ_HTTP/httpreact/src/App.jsx
+++ 7_REQ_HTTP/httpreact/src/App.jsx
@@ -6,6 +6,9 @@ const url = "http://localhost:3000/products"
 function App() {
   const [products, setProducts] = useState([])
 
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+
   //1- RESGATANDO DADOS
   useEffect(() => {
     
@@ -21,6 +24,26 @@ function App() {
 
   }, [])
 
+
+
+  //add produtos/dados
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const product = {
+      name,
+      price
+    }
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "aplication/json"
+      },
+      body:JSON.stringify(product)
+    })
+  }
+
   
 
   return (
@@ -32,6 +55,23 @@ function App() {
           <li key={product.id}> {product.name} - R${product.price}</li>
         )}
       </ul>
+
+
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input type="text" value={name} name="name" onChange={(e) => setName(e.target.value)} />
+          </label>
+
+          <label>
+            Preço:
+            <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)} />
+          </label>
+
+          <input type="submit" value="Criar" />
+        </form>
+      </div>
 
     </div>
   )
