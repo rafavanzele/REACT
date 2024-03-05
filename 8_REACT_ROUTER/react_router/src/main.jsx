ef2783d4504127ad572755357f9df1a51c13600f
rafavanzele <rafa_vanzele@hotmail.com>
2024-03-05 17:03:19 -0300

    melhorando css do navbar

--- 8_REACT_ROUTER/react_router/src/main.jsx
+++ 8_REACT_ROUTER/react_router/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import './components/Navbar.css'
 
 //1- Configurando o router
 import {createBrowserRouter, Navigate, RouterProvider} from 'react-router-dom'
